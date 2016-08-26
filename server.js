a0c0efeffa5d29bfed5c299dd41ee56cb3f5577f
Guillermo camarena <luismemo22@gmail.com>
2016-08-26 13:23:23 -0500

    mongo tutorial

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser= require('body-parser') 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/')
+  
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
